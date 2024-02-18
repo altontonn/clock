fe40c9a7fbbb23b62892e45527da9e0f8bd45f49
altontonn <altontonnalumasa@gmail.com>
2024-02-18 21:52:02 +0300

    Create methods to increment and decrement session length

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ class App extends React.Component {
     super(props);
     this.state = {
       break_length: 5,
-      session_length: 25,
+      session_length: 25 * 60,// converted minutes to seconds
     };
     this.increment_break_length = this.increment_break_length.bind(this);
+    this.increment_session_length = this.increment_session_length.bind(this);
     this.decrement_break_length = this.decrement_break_length.bind(this);
+    this.decrement_session_length = this.decrement_session_length.bind(this);
     // this.reset = this.reset.bind(this)
   }
   increment_break_length() {
@@ -17,10 +19,25 @@ class App extends React.Component {
       break_length: state.break_length + 1,
     }));
   }
+  increment_session_length() {
+    this.setState((state) => ({
+      session_length: state.session_length + 60,// increasing by 1 minute
+    }));
+  }
   decrement_break_length() {
     this.setState((state) => ({
       break_length: state.break_length > 1 ? state.break_length - 1 : 1,
     }));
+  }
+  decrement_session_length() {
+    this.setState((state) => ({
+      session_length: state.session_length > 60 ? state.session_length - 60 : 60,
+    }));
+  }
+  formatTime(seconds) {
+    const minutes = Math.floor(seconds/60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes.toString().padStart(2, 0)}: ${remainingSeconds.toString().padStart(2, 0)}`
   }
   render() {
     return (
@@ -50,20 +67,20 @@ class App extends React.Component {
         </div>
         <div className="length-control">
           <div id="session-label">Session Length</div>
-          <button className="btn-level" id="session-decrement" value="-">
+          <button className="btn-level" id="session-decrement" onClick={this.decrement_session_length} value="-">
             <i className="fa fa-arrow-down fa-2x"></i>
           </button>
           <div className="btn-level" id="session-length">
-            25
+            {Math.floor(this.state.session_length)/60} {/*convert seconds to minutes*/}
           </div>
-          <button className="btn-level" id="session-increment" value="+">
+          <button className="btn-level" id="session-increment" onClick={this.increment_session_length} value="+">
             <i className="fa fa-arrow-up fa-2x"></i>
           </button>
         </div>
         <div className="timer">
           <div className="timer-wrapper">
             <div id="timer-label">Session</div>
-            <div id="time-left">25:00</div>
+            <div id="time-left">{this.formatTime(this.state.session_length)}</div>
           </div>
         </div>
         <div className="timer-control">
